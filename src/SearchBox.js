ed32506feb3968da26b30da0dd08e72f7320027d
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-09-25 22:34:16 +0000

    Search functionality

---
+++ src/SearchBox.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class SearchBox extends Component {
+  render() {
+    const {searchfield, searchChange} = this.props;
+    return (
+      <div className='pa2'>
+        <input 
+          className='pa3 ba b--green bg-lightest-blue'
+          type='search' 
+          placeholder='search robots'
+          value={searchfield}
+          onChange={searchChange} 
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
