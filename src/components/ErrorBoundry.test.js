71982fbd30a0fb029ce84c497097dfef2015476b
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-12-09 21:48:24 +0000

    ErrorBoundry tests

---
+++ src/components/ErrorBoundry.test.js
@@ -0,0 +1,21 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import ErrorBoundry from './ErrorBoundry';
+
+describe('ErrorBoundry component', () => {
+  it('expects to render ErrorBoundry component', () => {
+    expect(shallow(<ErrorBoundry />)).toMatchSnapshot();
+  });
+
+  it('expects Errorboundry to have correct initial state', () => {
+    const wrapper = shallow(<ErrorBoundry />);
+    expect(wrapper.instance().state.hasError).toBe(false);
+  });
+
+  it('expects hasError to be true when there is an error', () => {
+    const wrapper = shallow(<ErrorBoundry />);
+    expect(wrapper.instance().state.hasError).toBe(false);
+    wrapper.instance().componentDidCatch();
+    expect(wrapper.instance().state.hasError).toBe(true);
+  });
+})
