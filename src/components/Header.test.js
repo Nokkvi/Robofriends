f7177d9e5e2f70061dc0d5dc0882cb4faafe9569
Nokkvi Sverrisson <nokkvisverriss@gmail.com>
2018-12-09 23:59:03 +0000

    Finished test coverage on Header

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -5,3 +5,8 @@ import Header from './Header';
 it('expects to render Header component', () => {
   expect(shallow(<Header />)).toMatchSnapshot();
 })
+
+it('expects shouldComponentUpdate to return false', () => {
+  const wrapper = shallow(<Header />);
+  expect(wrapper.instance().shouldComponentUpdate('', '')).toBe(false);
+});
